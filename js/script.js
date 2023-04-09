78725884be347c37d031c7a88026d612b8afb8d1
Musa Kamara <kamaramusa56@gmail.com>
2023-04-09 22:15:03 +0300

    I have done the footer

--- js/script.js
+++ js/script.js
@@ -16,47 +16,38 @@ document.querySelectorAll(".nav-link").forEach((n) =>
   })
 );
 
-
 //// ********Slide-bar-Js *********
 
-const ImgArray = document.querySelectorAll('.img')
+const ImgArray = document.querySelectorAll(".img");
 
-const rightBtn=document.getElementById('btn-right')
+const rightBtn = document.getElementById("btn-right");
 
+const leftBtn = document.getElementById("btn-left");
 
-const leftBtn=document.getElementById('btn-left')
-
-
-ImgArray.forEach(function(item,index){
-  item.style.left=`${index*100}%`;
+ImgArray.forEach(function (item, index) {
+  item.style.left = `${index * 100}%`;
 });
 
 let counter = 0;
 
+rightBtn.addEventListener("click", function () {
+  counter++;
+  transf();
+});
 
-rightBtn.addEventListener('click',function(){
-counter++;
-transf()
-})
-
-leftBtn.addEventListener('click',function(){
+leftBtn.addEventListener("click", function () {
   counter--;
-  transf()
-  })
+  transf();
+});
 
-
-
-function transf (){
-
-  if (counter===ImgArray.length){
-    counter=0
+function transf() {
+  if (counter === ImgArray.length) {
+    counter = 0;
   }
-  if (counter<0){
-
-    counter=ImgArray.length-1
+  if (counter < 0) {
+    counter = ImgArray.length - 1;
   }
-  ImgArray.forEach(function(item){
-    item.style.transform=`translateX(-${counter * 100}%)`
-  })
+  ImgArray.forEach(function (item) {
+    item.style.transform = `translateX(-${counter * 100}%)`;
+  });
 }
-
