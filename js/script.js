f7978f7ce6c6bcba68106e257c92655d61b37d5d
aminisd <aminisudi0@gmail.com>
2023-04-22 19:34:59 +0300

    Stand alone slider => Responsivness & test

--- js/script.js
+++ js/script.js
@@ -18,41 +18,6 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 
 //// ********Slide-bar-Js *********
 
-const ImgArray = document.querySelectorAll(".img");
-
-const rightBtn=document.getElementById('btn-righte')
-
-const leftBtn=document.getElementById('btn-lefte')
-
-
-ImgArray.forEach(function(item,index){
-  item.style.left=`${index*100}%`;
-});
-
-let counter = 0;
-
-rightBtn.addEventListener("click", function () {
-  counter++;
-  transf();
-});
-
-leftBtn.addEventListener("click", function () {
-  counter--;
-  transf();
-});
-
-function transf() {
-  if (counter === ImgArray.length) {
-    counter = 0;
-  }
-  if (counter < 0) {
-    counter = ImgArray.length - 1;
-  }
-  ImgArray.forEach(function (item) {
-    item.style.transform = `translateX(-${counter * 100}%)`;
-  });
-}
-
 // what other say slider
 
 const people = [
@@ -212,6 +177,153 @@ popu.addEventListener('click',function(){
     
   })
 
+
+
+  // Slider JS
+  // Modified cconceicao's slider (Amini Version )
+
+var slider = document.getElementById('slider'),
+sliderItems = document.getElementById('slides'),
+prev = document.getElementById('prev'),
+next = document.getElementById('next');
+
+function slide(wrapper, items, prev, next) {
+var posX1 = 0,
+  posX2 = 0,
+  posInitial,
+  posFinal,
+  threshold = 100,
+  slides = items.getElementsByClassName('slide'),
+  slidesLength = slides.length,
+  slideSize = document.getElementsByClassName('wrapper')[0].offsetWidth,
+  firstSlide = slides[0],
+  lastSlide = slides[slidesLength - 1],
+  cloneFirst = firstSlide.cloneNode(true),
+  cloneLast = lastSlide.cloneNode(true),
+  index = 0,
+  allowShift = true;
+
+// Clone first and last slide
+items.appendChild(cloneFirst);
+items.insertBefore(cloneLast, firstSlide);
+wrapper.classList.add('loaded');
+
+// Mouse events
+items.onmousedown = dragStart;
+
+// Touch events
+items.addEventListener('touchstart', dragStart);
+items.addEventListener('touchend', dragEnd);
+items.addEventListener('touchmove', dragAction);
+
+// Click events
+prev.addEventListener('click', function () { shiftSlide(-1); console.log('prev') });
+next.addEventListener('click', function () { shiftSlide(1);
+console.log('next') });
+
+// Transition events
+items.addEventListener('transitionend', checkIndex);
+
+setWidth();
+
+function dragStart (e) {
+e = e || window.event;
+e.preventDefault();
+posInitial = items.offsetLeft;
+
+if (e.type == 'touchstart') {
+  posX1 = e.touches[0].clientX;
+} else {
+  posX1 = e.clientX;
+  document.onmouseup = dragEnd;
+  document.onmousemove = dragAction;
+}
+}
+
+function dragAction (e) {
+e = e || window.event;
+
+if (e.type == 'touchmove') {
+  posX2 = posX1 - e.touches[0].clientX;
+  posX1 = e.touches[0].clientX;
+} else {
+  posX2 = posX1 - e.clientX;
+  posX1 = e.clientX;
+}
+items.style.left = (items.offsetLeft - posX2) + "px";
+}
+
+function dragEnd (e) {
+posFinal = items.offsetLeft;
+if (posFinal - posInitial < -threshold) {
+  shiftSlide(1, 'drag');
+} else if (posFinal - posInitial > threshold) {
+  shiftSlide(-1, 'drag');
+} else {
+  items.style.left = (posInitial) + "px";
+}
+
+document.onmouseup = null;
+document.onmousemove = null;
+}
+
+function shiftSlide(dir, action) {
+items.classList.add('shifting');
+
+if (allowShift) {
+  if (!action) { posInitial = items.offsetLeft; }
+
+  if (dir == 1) {
+    items.style.left = (posInitial - slideSize) + "px";
+    index++;      
+  } else if (dir == -1) {
+    items.style.left = (posInitial + slideSize) + "px";
+    index--;      
+  }
+};
+
+allowShift = false;
+}
+
+function checkIndex (){
+items.classList.remove('shifting');
+
+if (index == -1) {
+  items.style.left = -(slidesLength * slideSize) + "px";
+  index = slidesLength - 1;
+}
+
+if (index == slidesLength) {
+  items.style.left = -(1 * slideSize) + "px";
+  index = 0;
+}
+
+allowShift = true;
+}
+}
+
+slide(slider, sliderItems, prev, next);
+
+
+function setWidth(){
+
+
+var slider = document.getElementById('slider');
+
+var wrapper = document.getElementsByClassName('wrapper');
+
+var slides = document.getElementsByClassName('slide');
+
+
+Array.from(slides).forEach((item) => {
+item.style.width = wrapper[0].offsetWidth+ "px";
+
+});
+
+// slider.style.left = -(wrapper[0].offsetWidth)+'px';
+}
+
+window.addEventListener("resize", setWidth);
 
   
 
