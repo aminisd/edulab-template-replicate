ef2ba4875b6d716194b1d30250b3c8cecddbb68b
aminisd <aminisudi0@gmail.com>
2023-06-25 18:52:09 +0300

    navbar js

---
+++ src/js/navbar/navbar.js
@@ -0,0 +1,16 @@
+// responsive navbar
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.Navheader');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links').forEach((n) =>
+    n.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    }),
+);
--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,7 @@
 // Main javascript file.
 
 // responsive navbar javascript part
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.Navheader');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
-
-document.querySelectorAll('.nav-links').forEach((n) =>
-    n.addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('active');
-    }),
-);
+import './navbar/navbar';
 
 //// ********Slide-bar-Js *********
 
